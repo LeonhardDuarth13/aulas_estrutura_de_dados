4c633a898328f14236fcf3353774a7b1990f9de8
leo <leo.duarth13@gmail.com>
2020-02-21 17:04:31 -0300

    SOMA

---
+++ chama.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main (){
+    char op;
+    int operando1, operando2;
+    int resultado;
+    printf("Qual operação realizar\n");
+    scanf("%c", &op);
+    scanf("%d", &operando1);
+    printf("qual o segundo operando?\n");
+    scanf("%d", &operando2);
+
+    switch (op)
+    {
+    case '+':
+        printf("%d+%d = %d\n", operando1 , operando2, operando1 + operando2);
+        break;
+    }
+
+    return 0;
+}
+
+
+
+
